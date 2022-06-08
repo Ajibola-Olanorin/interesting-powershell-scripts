906d519ab48ce0821df416e31951a88ec6f8ea35
Ajibola-Olanorin <ajibolao365@gmail.com>
2022-06-08 19:55:57 +0100

    initial commit

---
+++ List-UnusedFiles.ps1
@@ -0,0 +1,15 @@
+﻿param([string]$DirTree = "C:\Users\HP Probook 440\Downloads", [int]$Days = 100)
+
+write-host "Listing files in $DirTree with last access time older than $Days days"
+
+try {
+	$cutOffDate = (Get-Date).AddDays(-$Days)
+
+	Get-ChildItem -path $DirTree -recurse | Where-Object {$_.LastAccessTime -le $cutOffDate} | select fullname
+
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
+
